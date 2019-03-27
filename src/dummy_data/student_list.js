de649c05710aef1001300bb32a3d4572e90dad79
Davideuijinlee <Davideuijinlee@gmail.com>
2019-03-27 12:54:25 -0700

    Completed display student

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'Clark Ken',
+        course: 'Kenology',
+        grade: '51',
+        id: 2
+    },
+    {
+        name: 'Ken',
+        course: 'Ken fu',
+        grade: '55',
+        id: 5
+    },
+    {
+        name: 'Kenneth',
+        course: 'Kentography',
+        grade: '60',
+        id: 8
+    },
+    {
+        name: 'Kenny',
+        course: 'Kenistry',
+        grade: '60',
+        id: 9
+    },
+    {
+        name: 'Kenneth Li',
+        course: 'Kenning',
+        grade: '80',
+        id: 10
+    }
+];
